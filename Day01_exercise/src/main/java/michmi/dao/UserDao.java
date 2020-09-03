6413351550ca7aa98fc00acfc6afadf971071454
michmi284 <58760931+michmi284@users.noreply.github.com>
2020-09-03 16:13:13 +0800

    111

---
+++ Day01_exercise/src/main/java/michmi/dao/UserDao.java
@@ -0,0 +1,12 @@
+package michmi.dao;
+
+import michmi.domain.User;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+
+public interface UserDao
+{@Select("select * from user")
+    public List<User>findAll();
+
+}
