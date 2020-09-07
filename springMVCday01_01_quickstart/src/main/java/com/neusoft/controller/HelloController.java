828b846c385357c521231a7ed0a9eb4bb3c4a8d0
michmi284 <58760931+michmi284@users.noreply.github.com>
2020-09-07 13:21:35 +0800

    111

--- springMVCday01_01_quickstart/src/main/java/com/neusoft/controller/HelloController.java
+++ springMVCday01_01_quickstart/src/main/java/com/neusoft/controller/HelloController.java
@@ -1,5 +1,15 @@
 package com.neusoft.controller;
 
-public class HelloController
-{
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(path = "/user")
+public class HelloController {
+
+    @RequestMapping(path = "/hello")
+    public String sayHello(){
+        System.out.println("Hello Controller ");
+        return "success";
+    }
 }
